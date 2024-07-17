bf28305d4f836b0121251a66e6555bad1c5d1e5b
Liewqr <166774431+Liewqr@users.noreply.github.com>
2024-07-17 22:36:27 +0800

    Egg catcher.py
    Added text messages

--- Egg catcher.py
+++ Egg catcher.py
@@ -2,15 +2,18 @@ from itertools import cycle
 from random import randrange
 from tkinter import Canvas, Tk, messagebox, font
 
+#Builidng the Canvas box 
 canvas_width = 900
 canvas_height = 400
 
+#Creating color of the canvas 
 root = Tk()
 c = Canvas(root, width=canvas_width, height=canvas_height, background="black")
 c.create_rectangle(-5, canvas_height-100, canvas_width+5, canvas_height+5, fill="orange", width=0)
 c.create_oval(-80, -80, 120, 120, fill='black', width=0)
 c.pack()
 
+#Changig the color cycle,egg shape,score,speed,color,difficulty,color catcher,catcher size,catcher starting position.
 color_cycle = cycle(["light blue", "light green", "light pink", "light yellow", "light cyan"])
 egg_width = 45
 egg_height = 55
@@ -26,19 +29,23 @@ catcher_starty = canvas_height - catcher_height - 20
 catcher_startx2 = catcher_startx + catcher_width
 catcher_starty2 = catcher_starty + catcher_height
 
+#Creating the catcher shape
 catcher = c.create_arc(catcher_startx, catcher_starty, catcher_startx2, catcher_starty2, start=200, extent=140, style="arc", outline=catcher_color, width=3)
 game_font = font.nametofont("TkFixedFont")
 game_font.config(size=18)
 
-
+#Adding Catching score
 score = 0
 score_text = c.create_text(10, 10, anchor="nw", font=game_font, fill="white", text="Score: "+ str(score))
 
+#Add in the lives
 lives_remaining = 3
 lives_text = c.create_text(canvas_width-10, 10, anchor="ne", font=game_font, fill="white", text="Lives: "+ str(lives_remaining))
 
+#Creating Egg list
 eggs = []
 
+#Added creating eggs
 def create_egg():
     x = randrange(10, 740)
     y = 40
@@ -46,6 +53,7 @@ def create_egg():
     eggs.append(new_egg)
     root.after(egg_interval, create_egg)
 
+#Added Moving Eggs
 def move_eggs():
     for egg in eggs:
         (eggx, eggy, eggx2, eggy2) = c.coords(egg)
@@ -54,6 +62,7 @@ def move_eggs():
             egg_dropped(egg)
     root.after(egg_speed, move_eggs)
 
+#Added Eggs to drop 
 def egg_dropped(egg):
     eggs.remove(egg)
     c.delete(egg)
@@ -62,11 +71,13 @@ def egg_dropped(egg):
         messagebox.showinfo("Game Over!", "Skill issue,Final Score: "+ str(score))
         root.destroy()
 
+#Added losing a life
 def lose_a_life():
     global lives_remaining
     lives_remaining -= 1
     c.itemconfigure(lives_text, text="Lives: "+ str(lives_remaining))
 
+#Adding Catch checker
 def check_catch():
     (catcherx, catchery, catcherx2, catchery2) = c.coords(catcher)
     for egg in eggs:
@@ -77,6 +88,7 @@ def check_catch():
             increase_score(egg_score)
     root.after(100, check_catch)
 
+#Adding increasing the score everytime the egg catches
 def increase_score(points):
     global score, egg_speed, egg_interval
     score += points
@@ -84,16 +96,19 @@ def increase_score(points):
     egg_interval = int(egg_interval * difficulty)
     c.itemconfigure(score_text, text="Score: "+ str(score))
 
+#Adding moving to the left
 def move_left(event):
     (x1, y1, x2, y2) = c.coords(catcher)
     if x1 > 0:
         c.move(catcher, -20, 0)
-
+        
+#Adding moving to the right
 def move_right(event):
     (x1, y1, x2, y2) = c.coords(catcher)
     if x2 < canvas_width:
         c.move(catcher, 20, 0)
 
+#create the left and right keys and adding the delay 
 c.bind("<Left>", move_left)
 c.bind("<Right>", move_right)
 c.focus_set()
